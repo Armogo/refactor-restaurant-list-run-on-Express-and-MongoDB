bf9759419f5a718b0bd5cdd2fb436be61e1018d4
Armogo <taro37940@yahoo.com.tw>
2021-07-08 15:56:44 +0800

    Complete search bar function and use regex to avoid bad UX caused by whitspace in the search bar

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const rstrts = require('./restaurant.json')
+const rstrtList = require('./restaurant.json')
 
 // set up template engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
@@ -14,16 +14,24 @@ app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index', {rstrts: rstrts.results})
+  res.render('index', {rstrts: rstrtList.results})
 })
 
 // show specific restaurant details
 app.get('/restaurants/:id', (req, res) => {
-  const rstrt = rstrts.results.find(rstrt => rstrt.id.toString() === req.params.id)
+  const rstrt = rstrtList.results.find(rstrt => rstrt.id.toString() === req.params.id)
   res.render('show', {rstrt: rstrt})
 })
 
 // search bar
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.replace(/ +/g, "")
+  const rstrts = rstrtList.results.filter(rstrt => {
+    return rstrt.category.toLowerCase().includes(keyword.toLowerCase()) ||
+    rstrt.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', {rstrts: rstrts, keyword: keyword})
+})
 
 // start and listen on the Express server
 app.listen(port, () => {
