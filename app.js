30d59f6782e589c619da8f6d9b93b997d99722bb
Armogo <taro37940@yahoo.com.tw>
2021-07-11 22:33:43 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const rstrtList = require('./restaurant.json')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true }) // connect to mongoDB
+
+// status of connection
+const db = mongoose.connection
+// error message
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// connection succeeds
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // set up template engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
