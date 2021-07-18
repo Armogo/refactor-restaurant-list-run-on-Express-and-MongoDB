fac880434fb9864c734a4d9d8bc9bdb1246b933d
Armogo <taro37940@yahoo.com.tw>
2021-07-18 22:59:27 +0800

    feat: add sort function and modify layout of home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -12,5 +12,37 @@ router.get('/', (req, res) => {
     .catch(error => console.log(error)) // 錯誤處理
 })
 
+router.get('/sort-asc', (req, res) => {
+  return Restaurant.find() // 取出 restaurant model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ name: 'asc' }) // 照名稱升序排列
+    .then(rstrts => res.render('index', { rstrts })) // 將資料傳給 index 樣板
+    .catch(error => console.log(error)) // 錯誤處理
+})
+
+router.get('/sort-desc', (req, res) => {
+  return Restaurant.find() // 取出 restaurant model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ name: 'desc' }) // 照名稱降序排列
+    .then(rstrts => res.render('index', { rstrts })) // 將資料傳給 index 樣板
+    .catch(error => console.log(error)) // 錯誤處理
+})
+
+router.get('/sort-category', (req, res) => {
+  return Restaurant.find() // 取出 restaurant model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({category: 'asc' }) // 照種類排列
+    .then(rstrts => res.render('index', { rstrts })) // 將資料傳給 index 樣板
+    .catch(error => console.log(error)) // 錯誤處理
+})
+
+router.get('/sort-location', (req, res) => {
+  return Restaurant.find() // 取出 restaurant model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({location: 'asc' }) // 照地址排列
+    .then(rstrts => res.render('index', { rstrts })) // 將資料傳給 index 樣板
+    .catch(error => console.log(error)) // 錯誤處理
+})
+
 // 匯出路由器
 module.exports = router
